15ba331825ccadd9f0ffcf7462da7b5ac1545a3a
RichiePalma <palmamendoza.5@hotmail.com>
2017-03-31 14:04:19 -0600

    Add files via upload

---
+++ js/Examen.js
@@ -0,0 +1,8 @@
+function mostrarTexto(){
+  if(document.getElementById('respuesta').style.display == "none"){
+      document.getElementById('respuesta').style.display = "block";
+  }
+  else{
+      document.getElementById('respuesta').style.display = "none";
+  }
+}
